a6e82afe2513fb47331d890cfc08c4853126b46c
Tran Thanh Vinh <kkiras.xd@gmail.com>
2023-12-12 07:59:36 +0700

    1

--- app/src/main/java/vlu/mobileproject/globalfuction/GlobalData.java
+++ app/src/main/java/vlu/mobileproject/globalfuction/GlobalData.java
@@ -64,8 +64,6 @@ public class GlobalData {
 //                        else {
 //
 //                        }
-
-
                     }
                     callBack.onCompleted(products);
                 }
